00460f0860b145c55d8ca0509d921ab16c906413
Pawissakan Chirupphapa <pawissakan.cpcu@gmail.com>
2018-03-23 13:04:16 +0700

    Implement Order model with sqlite3

--- bot/order.py
+++ bot/order.py
@@ -1,13 +1,63 @@
 # @Description : Structures for keeping orders from users.
 
-class OrderPool:
-    def __init__(self):
-        pass
+import sqlite3
 
-class OrderList:
-    def __init__(self):
-        pass
+DEFAULT_DB_PATH = "/tmp/preo-bot.db"
+
+class OrderQuery:
+    INIT_SCHEMA = """
+            CREATE TABLE IF NOT EXISTS orders (
+            room_id  CHAR(128) NOT NULL,
+			user_id CHAR(128) NOT NULL,
+			item_name VARCHAR(255) NOT NULL,
+			amount INT NOT NULL,
+			CONSTRAINT PK_Order PRIMARY KEY (room_id, user_id, item_name));
+            """
+
+    SET_ORDER = """
+        INSERT OR REPLACE INTO orders (room_id, user_id, item_name, amount)
+        VALUES (?,?,?,?)"""
+
+    DEL_ORDER = "DELETE FROM orders WHERE room_id = ? and user_id = ? and item_name = ?"
+
+    SELECT_ALL_ORDER = "SELECT * FROM orders"
+    SELECT_ORDER_BY_ROOM = "SELECT * FROM orders WHERE room_id = ?"
+    SELECT_ORDER_BY_USER = "SELECT * FROM orders WHERE room_id = ? and user_id = ?"
+    SELECT_ORDER_BY_ITEM = "SELECT *, count(amount) FROM orders WHERE room_id = ? and item_name = ?"
 
 class Order:
-    def __init__(self):
+    "Order model for managing orders table"
+
+    def __init__(self, db_path=DEFAULT_DB_PATH):
+        self.db = sqlite3.connect(db_path)
+        self.__create_schema()
+
+    def __create_schema(self):
+        cursor = self.db.cursor()
+        cursor.execute(OrderQuery.INIT_SCHEMA)
+        self.db.commit()
+
+    def set_order(self, room_id, user_id, item_name, amount):
+        cursor = self.db.cursor()
+        cursor.execute(OrderQuery.SET_ORDER, [room_id, user_id, item_name, amount])
+        self.db.commit()
+
+    def del_order(self, room_id, user_id, item_name):
+        cursor = self.db.cursor()
+        cursor.execute(OrderQuery.DEL_ORDER, [room_id, user_id, item_name])
+        self.db.commit()
+
+    def list_all(self):
+        cursor = self.db.cursor()
+        cursor.execute(OrderQuery.SELECT_ALL_ORDER)
+        rows = cursor.fetchall()
+        return rows
+
+    def get_room_order(self, room_id):
+        pass
+
+    def get_user_order(self, room_id, user_id):
+        pass
+
+    def get_item_order(self, room_id, item_name):
         pass
--- tests/test_order.py
+++ tests/test_order.py
@@ -7,35 +7,57 @@ parentdir = os.path.dirname(currentdir)
 # Include paths for module search
 sys.path.insert(0, os.path.join(parentdir, 'bot'))
 from order import (
-    OrderPool, OrderList, Order
+    Order
 )
 
-###########################
-# OrderPool test cases
-###########################
-
-
-def test_order_pool_init():
-    order_pool = OrderPool()
-    assert order_pool != None
-    assert isinstance(order_pool, OrderPool)
-
-###########################
-# OrderList test cases
-###########################
-
-
-def test_order_list_init():
-    order_list = OrderList()
-    assert order_list != None
-    assert isinstance(order_list, OrderList)
+TEST_DB_PATH = "/tmp/test-preo-bot.db"
 
 ###########################
 # Order test cases
 ###########################
 
 
+MOCK_ORDERS = [
+    ['10001', '20001', 'milk', 1],
+    ['10001', '20001', 'steak', 1],
+    ['10001', '20002', 'milk', 1],
+    ['10001', '20003', 'milk', 1],
+    ['10001', '20004', 'bread', 2],
+    ['10001', '20004', 'milk', 1]
+]
+
+
+def clean_db():
+    print("Remove DB at %s" % (TEST_DB_PATH))
+    os.remove(TEST_DB_PATH)
+
+
+def insert_mock_data(order):
+    for data in MOCK_ORDERS:
+        order.set_order(data[0], data[1], data[2], data[3])
+
+
 def test_order_init():
-    order = Order()
+    clean_db()
+    order = Order(TEST_DB_PATH)
     assert order != None
     assert isinstance(order, Order)
+
+
+def test_order_set_order():
+    clean_db()
+    order = Order(TEST_DB_PATH)
+    for data in MOCK_ORDERS:
+        order.set_order(data[0], data[1], data[2], data[3])
+    rows = order.list_all()
+    assert len(rows) == len(MOCK_ORDERS)
+
+
+def test_order_del_order():
+    clean_db()
+    order = Order(TEST_DB_PATH)
+    insert_mock_data(order)
+    for data in MOCK_ORDERS:
+        order.del_order(data[0], data[1], data[2])
+    rows = order.list_all()
+    assert len(rows) == 0
